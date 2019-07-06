854f6ed25cb16f59b8795d10617e3bd9396e6d64
hasini <hasinikeshala123@gmail.com>
2019-07-06 16:50:38 +0530

    final commit

---
+++ src/main/java/pizzaloop/OrderRepository.java
@@ -0,0 +1,18 @@
+package pizzaloop;
+
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Transactional
+public interface OrderRepository extends CrudRepository<OrderDetails, Integer>{
+    /*
+     * Find pizza by Id
+     */
+    List<OrderDetails> findByCartId(Integer id);
+
+    /*
+     * Delete pizza by Id
+     */
+    List<OrderDetails> deleteByCartId(Integer id);
+}
